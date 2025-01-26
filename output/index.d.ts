0cdbb8e36d8f4f5d62312552095e7aedae2fccad
unbywyd <unbywyd@gmail.com>
2025-01-26 08:21:45 +0200

    +

--- output/index.d.ts
+++ output/index.d.ts
@@ -1,8 +1,8 @@
 import type { Application } from 'express';
 import { Server as httpServer } from "http";
-import type { Container } from 'typedi';
 import { ServerOptions, Socket } from "socket.io";
 import type { SocketControllers } from "socket-controllers";
+import type { AppPlugin, AppContext } from 'tsdiapi-server';
 export type SocketSuccessResponse<T> = {
     status: "ok";
     data?: T;
@@ -26,31 +26,6 @@ export type PluginOptions = {
     socketOptions?: Partial<ServerOptions>;
     socketControllers: typeof SocketControllers;
 };
-export interface AppOptions {
-    appConfig: any;
-    environment: string;
-    corsOptions: any;
-    swaggerOptions: any;
-    [key: string]: any;
-}
-export interface PluginContext {
-    appDir: string;
-    apiDir: string;
-    app: Application;
-    server?: httpServer;
-    plugins: Record<string, AppPlugin>;
-    container: typeof Container;
-    config: AppOptions;
-    logger: any;
-}
-export interface AppPlugin {
-    name: string;
-    bootstrapFilesGlobPath?: string;
-    appConfig?: Record<string, any>;
-    onInit?(ctx: PluginContext): Promise<void> | void;
-    beforeStart?(ctx: PluginContext): Promise<void> | void;
-    afterStart?(ctx: PluginContext): Promise<void> | void;
-}
 export type SocketEvents = {
     [key: string]: {
         payload: any;
@@ -61,10 +36,10 @@ export default class App implements AppPlugin {
     name: string;
     config: PluginOptions;
     globFilesPath: string;
-    context: PluginContext;
+    context: AppContext;
     constructor(config?: PluginOptions);
     registerSocketControllers(app: Application, server: httpServer): Promise<void>;
-    onInit(ctx: PluginContext): Promise<void>;
-    beforeStart(ctx: PluginContext): Promise<void>;
+    onInit(ctx: AppContext): Promise<void>;
+    beforeStart(ctx: AppContext): Promise<void>;
 }
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type { Container } from 'typedi';
 import { Server as SocketIOServer, ServerOptions, Socket } from "socket.io";
 import { glob } from 'glob';
 import type { SocketControllers } from "socket-controllers";
+import type { AppPlugin, AppContext } from 'tsdiapi-server';
 export type SocketSuccessResponse<T> = {
     status: "ok";
     data?: T;
@@ -98,33 +99,6 @@ const defaultConfig: Partial<PluginOptions> = {
     }
 }
 
-export interface AppOptions {
-    appConfig: any;
-    environment: string;
-    corsOptions: any;
-    swaggerOptions: any;
-    [key: string]: any;
-}
-
-export interface PluginContext {
-    appDir: string;
-    apiDir: string;
-    app: Application;
-    server?: httpServer;
-    plugins: Record<string, AppPlugin>;
-    container: typeof Container;
-    config: AppOptions;
-    logger: any
-}
-export interface AppPlugin {
-    name: string;
-    bootstrapFilesGlobPath?: string;
-    appConfig?: Record<string, any>;
-    onInit?(ctx: PluginContext): Promise<void> | void;
-    beforeStart?(ctx: PluginContext): Promise<void> | void;
-    afterStart?(ctx: PluginContext): Promise<void> | void;
-}
-
 export type SocketEvents = {
     [key: string]: {
         payload: any;
@@ -136,7 +110,7 @@ export default class App implements AppPlugin {
     name = 'tsdiapi-io';
     config: PluginOptions;
     globFilesPath: string;
-    context: PluginContext;
+    context: AppContext;
 
     constructor(config?: PluginOptions) {
         this.config = { ...config };
@@ -218,10 +192,10 @@ export default class App implements AppPlugin {
             }
         }
     }
-    async onInit(ctx: PluginContext) {
+    async onInit(ctx: AppContext) {
         this.context = ctx;
     }
-    async beforeStart(ctx: PluginContext) {
+    async beforeStart(ctx: AppContext) {
         this.registerSocketControllers(ctx.app, ctx.server);
     }
 }
