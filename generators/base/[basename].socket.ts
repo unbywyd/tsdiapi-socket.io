338e02c9eb1e687d7bfa89029e146e26426ec6fb
unbywyd <unbywyd@gmail.com>
2025-03-10 22:19:13 +0200

    +

--- generators/base/[basename].socket.ts
+++ generators/base/[basename].socket.ts
@@ -7,12 +7,32 @@ import {
   SocketController,
 } from "socket-controllers";
 import { Service } from "typedi";
+import { AuthAppSocket } from "@tsdiapi/socket.io";
 
-import { AppSocketType, SocketPayloads, SocketEvent } from "@base/sockets.types.js";
+export enum Socket{{className}}Event {
+  {{eventName}} = "helloWorld",
+}
 
+export interface Socket{{className}}Payloads {
+  [Socket{{className}}Event.{{eventName}}]: {};
+}
+
+export interface Socket{{className}}Responses {
+  [Socket{{className}}Event.{{eventName}}]: {
+    message: string;
+  };
+}
+
+export type AppSocketType = AuthAppSocket<
+  { userId: string },
+  Socket{{className}}Event,
+  Socket{{className}}Event,
+  Socket{{className}}Payloads,
+  Socket{{className}}Responses
+>;
 @SocketController()
 @Service()
-export default class {{ className }}Controller {
+export default class {{className}}Controller {
   @OnConnect()
   connection(@ConnectedSocket() socket: AppSocketType) {
     console.log("{{className}} connected");
@@ -23,13 +43,13 @@ export default class {{ className }}Controller {
     console.log("{{className}} disconnected");
   }
 
-  @OnMessage(SocketEvent.{{ eventName }})
-  async on{ {pascalCase eventName } } (
+  @OnMessage(SocketEvent.{{eventName}})
+  async on{{pascalCase eventName}} (
   @ConnectedSocket() socket: AppSocketType,
-    @MessageBody() message: SocketPayloads[SocketEvent.{ { eventName } }]
+    @MessageBody() message: SocketPayloads[SocketEvent.{{eventName}}]
   ) {
   console.log("Received {{eventName}} message:", message);
-  socket.emitSuccess(SocketEvent.{{ eventName }}, {
+  socket.emitSuccess(SocketEvent.{{eventName}}, {
   message: "{{className}} processed event {{eventName}}.",
     });
   }
